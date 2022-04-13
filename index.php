ac780c32c67d1553db897c68a3f06ed21bf4e379
Sergei Yurchenko <sergyurch@gmail.com>
2022-04-13 20:45:14 +0300

    Basic structure

--- index.php
+++ index.php
@@ -6,10 +6,21 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
     <body>
-        <header>Хедер</header>
-        <nav></nav>
-        <main></main>
-        <footer>Футер</footer>
+        <div class="container min-vh-100 d-flex flex-column">
+            <header class="p-5 text-center bg-primary">Header</header>
+            <nav class="p-3 bg-warning">
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Урок 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Урок 1</a>
+                    </li>
+                </ul>
+            </nav>
+            <main class="p-3 text-center flex-fill">Main</main>
+            <footer class="p-5 text-center bg-info">Footer</footer>
+        </div>
     </body>
 </html>
 
